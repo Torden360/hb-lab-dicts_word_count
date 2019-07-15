bf1e9005c54bece85372b05f931d51c0fe8dd9fc
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-15 12:27:49 -0700

    initial commit, created find word count function!

---
+++ wordcount.py
@@ -0,0 +1,26 @@
+# put your code here.
+
+def find_word_count(text_file):
+    word_count_dict = {}
+    test_file = open(text_file)
+
+    for line in test_file:
+        line = line.rstrip()
+        word_list = line.split(' ')
+        for word in word_list:
+            word = word.rstrip(",").rstrip(".").rstrip("?").lower()
+            word_count_dict[word] = word_count_dict.get(word, 0) + 1
+            # if not word.isalpha():
+            #     print(word)
+            #     word = word.rstrip()
+            #     print(word)
+            #     word = word.lower()
+            #     word_count_dict[word] = word_count_dict.get(word, 0) + 1
+            # else:
+            #     word = word.lower()
+            #     word_count_dict[word] = word_count_dict.get(word, 0) + 1
+    print(word_count_dict)
+
+# print(find_word_count("twain.txt"))
+print(find_word_count("test.txt"))
+
